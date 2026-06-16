34bbb55b4d06611ee3db75eb88b630f29c04b148
Qorina Qisthi Kamal <qorinaqisthykamaltubaba@gmail.com>
2026-06-16 15:52:18 +0700

    studfreelance fix UAS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,7 @@ dependencies {
 
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
